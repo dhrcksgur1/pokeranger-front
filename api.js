c6265322eb7f6272d5a6f444241c71b77cad80b7
[Cloud]황소현 <babystep9696@gmail.com>
2024-03-11 00:58:59 +0900

    삭제 del 요청 코드수정

--- api.js
+++ api.js
@@ -109,29 +109,35 @@ async function patch(endpoint, params = "", data) {
   return result;
 }
 
+
 // 아래 함수명에 관해, delete 단어는 자바스크립트의 reserved 단어이기에,
 // 여기서는 우선 delete 대신 del로 쓰고 아래 export 시에 delete로 alias 함.
-async function del(endpoint, params = "", data = {}) {
+async function del(endpoint, params = "") {
 
    const apiUrl = params ? `${baseUrl}${endpoint}/${params}` : `${baseUrl}${endpoint}`;
     console.trace('Current Call Stack: get ');
-   console.log(`%cGET 요청: ${apiUrl} `, "color: #a25cd1;");
+   console.log(`%cDEL 요청: ${apiUrl} `, "color: #a25cd1;");
 
-
-  console.log(data);
-  const bodyData = JSON.stringify(data);
 
   console.log(`DELETE 요청 ${apiUrl}`, "color: #059c4b;");
   console.log(`DELETE 요청 데이터: ${bodyData}`, "color: #059c4b;");
 
+
+  // 토큰이 있으면 Authorization 헤더를 포함, 없으면 포함하지 않음
+  const token = sessionStorage.getItem("token");
+  const headers = {
+    "Content-Type": "application/json",
+    ...(token && { Authorization: `Bearer ${token}` }),
+    };
+
+    console.log(token);
+    console.log(headers);
+
   const res = await fetch(apiUrl, {
     method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-    },
-    body: bodyData,
+    headers
   });
+
 
   // 응답 코드가 4XX 계열일 때 (400, 403 등)
   if (!res.ok) {
